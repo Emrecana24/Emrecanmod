dcab244b37492954e1cc6f473517d25b22574fd1
Emrecana24 <150844555+Emrecana24@users.noreply.github.com>
2026-01-03 18:00:18 +0000

    Remove example mod leftovers, add clean mixin infrastructure

---
+++ src/main/java/com/emrecan/cilent/EmrecanClient.java
@@ -0,0 +1,10 @@
+package com.emrecan.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmrecanClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // İleride client kodları buraya
+    }
+}
