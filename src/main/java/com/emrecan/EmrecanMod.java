4045e7b152ed11e615bc9d7d3558e8591d9826e8
Emrecana24 <150844555+Emrecana24@users.noreply.github.com>
2026-01-03 18:08:36 +0000

    Restore main mod entrypoint (EmrecanMod)

---
+++ src/main/java/com/emrecan/EmrecanMod.java
@@ -0,0 +1,13 @@
+package com.emrecan;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmrecanMod implements ModInitializer {
+
+    public static final String MOD_ID = "emrecan";
+
+    @Override
+    public void onInitialize() {
+        // Modun ana başlangıç noktası
+    }
+}
